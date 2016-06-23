2cce16352b917588cc1e9706bd0b035675aa9bbe
Andrew Bartuska <andrew.bartuska@duke.edu>
2016-06-23 10:23:29 -0400

    Small tweaks and comments for clarity

--- movie_maker.py
+++ movie_maker.py
@@ -9,16 +9,12 @@ import matplotlib.animation as animation
 import scipy.io as sio
 from scipy.signal import savgol_filter
 import os
+import sys
 
 import argparse
 
-# Local smooth on the x and y dimensions (numpy and pandas) a simple boxcar (average over a running window)
-
-# White dots on a black background
-
 # Required:
 # 	- Change the emotions variable in load_mat to match the MATLAB file that it comes from
-#	- Make 'movies' directory to store movies
 
 emotions = {1: 'angry',
 			2: 'disgust',
@@ -29,7 +25,8 @@ emotions = {1: 'angry',
 def load_mat(filename):
 	mat = sio.loadmat(filename)
 	landmarks = mat['landmarks']
-	emots = mat['emots']
+	emots = mat['emots_intraface']
+	# 'emots' in vid1/2 files and 'emots_intraface' or 'emots_jh' in rand_001/
 	landmarks = pd.DataFrame(landmarks)
 	
 	return landmarks, emots
@@ -50,12 +47,15 @@ def rle(emots):
 
 # segments = (run length, startpositions, values) of emotions
 
-def find_long_segments(segments, target_length):
+def find_long_segments(segments, target_length, neut_len):
 	long = []
 	for i in range(len(segments[0])):
 		
-		if segments[0][i] > target_length:
+		# Is segment of video long enough?
+		
+		if (segments[0][i] > target_length):
 			long.append((segments[0][i], segments[1][i], segments[2][i]))
+			
 	return long
 
 def setup_movie_writer():
@@ -86,7 +86,8 @@ def make_emotion_movie(landmarks, emots, filename, emotion_name, emotion_num, co
     	os.makedirs(newpath)
     
     name = filename+'_'+emotion_name+emotion_num+'.mp4'
-	
+    print name
+
     with writer.saving(fig, newpath+'/'+name, 300):
         for i in range(coords[1], coords[1]+coords[0]): #coords = (length, start, value)
             ax.set_xticks([])
@@ -95,7 +96,6 @@ def make_emotion_movie(landmarks, emots, filename, emotion_name, emotion_num, co
             ax.scatter(landmarks[i][0:48], landmarks[i][49:97], color='white')
             writer.grab_frame()
             ax.cla()
-	print name
 	
 def make_movies(landmarks, emots, long, writer, filename):
 	counters = np.ones(5) # angry, disgust, happy, sad, neutral
@@ -118,16 +118,54 @@ if __name__ == "__main__":
 	parser.add_argument('-tl', '--target_length', dest='target', type=int, default=30, 
 						help="Target length for emotion frames cutoff"
 						)
+	parser.add_argument('-n', '--neutral_length', dest='neut_len', type=int, default=0, 
+						help="Length of neutral before emotion"
+						)
 
 	args = parser.parse_args()
 	
 	for file in args.files:
 		print file
+		#if args.files.index(file) < args.files.index('rand_001/00120.mat'):
+		#	continue
+		
 		landmarks, emots = load_mat(file)
 		segments = rle(emots)
+		
 		if type(segments[0]) == type(None):
 			continue
-		long = find_long_segments(segments, args.target)
+		
+		long = find_long_segments(segments, args.target, args.neut_len)
+		if not any(long):
+			continue
+		
 		writer = setup_movie_writer()
 		filtered_data = filter_landmarks(landmarks)
 		make_movies(filtered_data, emots, long, writer, file)
+
+'''
+# Non-working version of finding neutral segments preceding video
+def find_long_segments(segments, target_length, neut_len):
+	long = []
+	for i in range(len(segments[0])):
+		
+		# Is segment of video long enough?
+		# And preceded by X seconds of neutral video? (Can comment out to just find emotion videos)
+		# Neut_len/fps (usually 15) is seconds of neutral video preceding
+		#if i < neut_len:
+		#	continue
+		
+		#neutral_amount = 0.
+		#for val in segments[2][i-neut_len:i]:
+		#	if val == 5.0:
+		#		neutral_amount += 1
+		
+		if (segments[0][i] > target_length): # and (neutral_amount/neut_len >= 0.5):
+			long.append((segments[0][i], segments[1][i], segments[2][i]))
+	
+	# (np.array_equal(segments[2][i-neut_len:i], np.full(neut_len, 5.))):
+	# Need to remove neut_len if commenting out preceding neutral video
+	# long.append((segments[0][i] + neut_len, segments[1][i-neut_len], segments[2][i]))	
+	
+	return long
+'''
