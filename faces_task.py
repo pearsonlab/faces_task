33fd9fed1571e9769a60895114b2b095b2339e38
Andrew Bartuska <andrew.bartuska@duke.edu>
2016-07-22 10:28:18 -0400

    Fixed no Audio

--- faces_task.py
+++ faces_task.py
@@ -60,7 +60,7 @@ def play_example(win, movie, timing):
 
 def play_movie(win, movie, timing):
 	mov = visual.MovieStim3(win, 'movies/'+movie, size=[1080,637.5],
-                       flipVert=False, flipHoriz=False, loop=False)
+                       flipVert=False, flipHoriz=False, loop=False, noAudio=True)
 	
 	timer = core.CountdownTimer(timing)
 	mov_start = core.getTime()
